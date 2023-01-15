8266efc9507cd3c343dc10913fde32baf9dfd8f7
SharonPeled <ssharon95@gmail.com>
2023-01-15 22:01:51 +0200

    reverting to full piepline

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -7,25 +7,11 @@ from ..configs import Configs
 
 
 
-# TODO: handle the MAG - not resizing, adding mag to metadata
 
-# TODO: otsu approx on small image!!
-# TODO: change to resize but only for tissue..
-# TODO: fix recover to not load from tile dir
-# TODO: deciding on resizing strategy - in addition to calc otsu on small - figureing out the bg on small
-# TODO: remove deprecated code
+
 # TODO: documenting all the tricks
 
-
-
-# TODO: check that the filtered background in the reuced slide makes sense.
-# TODO: creating new pipeline..
-
-
-# TODO: reordering the logs
-# TODO: reorder all redundant code
-
-
+# TODO: fine tuned the thresholds
 
 # TODO: try to multi thread
 # TODO: learn about slurm
@@ -39,8 +25,8 @@ def execute_preprocessing_pipeline(with_tiling):
         ('slide', Pipeline([
             ('load_slide', LoggingFunctionTransformer(load_slide,
                                                       kw_args={'load_level': Configs.REDUCED_LEVEL_TO_MEMORY})),
-            # ('scale_mpp', LoggingFunctionTransformer(resize, kw_args={'target_mag_power': Configs.TARGET_MAG_POWER,
-            #                                                           'mag_attr': Configs.MAG_ATTR})),
+            ('scale_mpp', LoggingFunctionTransformer(resize, kw_args={'target_mag_power': Configs.TARGET_MAG_POWER,
+                                                                      'mag_attr': Configs.MAG_ATTR})),
             ('load_reduced_image_to_memory', LoggingFunctionTransformer(load_reduced_image_to_memory)),
             ('center_crop_reduced_image', LoggingFunctionTransformer(center_crop_reduced_image,
                                                                      kw_args={'tile_size': Configs.TILE_SIZE,
@@ -59,10 +45,10 @@ def execute_preprocessing_pipeline(with_tiling):
         pipeline_list.append(
             ('tile', Pipeline([
                 ('load_tile', LoggingFunctionTransformer(load_tile)),
-                # ('macenko_color_norm', LoggingFunctionTransformer(macenko_color_norm,
-                #                                                   kw_args={'ref_img_path': Configs.COLOR_NORM_REF_IMG,
-                #                                                            'succ_norm_attr': Configs.COLOR_NORM_SUCC,
-                #                                                            'fail_norm_attr': Configs.COLOR_NORM_FAIL})),
+                ('macenko_color_norm', LoggingFunctionTransformer(macenko_color_norm,
+                                                                  kw_args={'ref_img_path': Configs.COLOR_NORM_REF_IMG,
+                                                                           'succ_norm_attr': Configs.COLOR_NORM_SUCC,
+                                                                           'fail_norm_attr': Configs.COLOR_NORM_FAIL})),
                 ('save_processed_tile', LoggingFunctionTransformer(save_processed_tile,
                                                                    kw_args={'processed_tiles_dir': Configs.PROCESSED_TILES_DIR,
                                                                             'fail_norm_attr': Configs.COLOR_NORM_FAIL}))
