700616904d40d33619e1810072b89b010f0d246b
SharonPeled <ssharon95@gmail.com>
2023-02-04 14:26:06 +0200

    adding bring tumor thumbnails option in main

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
     if args.bring_thumbnails:
         bring_files(Configs.SLIDES_DIR, 'thumbnail.png', args.bring_thumbnails)
     if args.bring_tumor_thumbnails:
-        bring_files(Configs.SLIDES_DIR, 'tumor_thumbnail.png', args.bring_thumbnails)
+        bring_files(Configs.SLIDES_DIR, 'tumor_thumbnail.png', args.bring_tumor_thumbnails)
     if args.bring_slide_logs:
         bring_joined_log_file(Configs.SLIDES_DIR, Configs.PROGRAM_LOG_FILE_ARGS[0], args.bring_slide_logs)
     if args.train_tumor_classifier:
