2d4d86286eca14d821ef9d56596c6e7eaa66de34
SharonPeled <ssharon95@gmail.com>
2023-03-29 16:55:38 +0300

    save commit

--- src/components/ProcessedTileDataset.py
+++ src/components/ProcessedTileDataset.py
@@ -4,8 +4,9 @@ import os
 import pandas as pd
 from PIL import Image
 from ..components.Logger import Logger
+from ..configs import Configs
 
-
+# ss_thumbnail_thrid_try_predict_224.png
 class ProcessedTileDataset(Dataset, Logger):
     def __init__(self, processed_tiles_dir, transform=None):
         self.processed_tiles_dir = processed_tiles_dir
@@ -13,6 +14,8 @@ class ProcessedTileDataset(Dataset, Logger):
         tile_paths = glob(f"{processed_tiles_dir}/**/*Tissue*.jpg", recursive=True)
         self.df = pd.DataFrame(tile_paths, columns=['tile_path'])
         self.df['slide_uuid'] = self.df.tile_path.apply(lambda p: os.path.basename(os.path.dirname(p)))
+        processed_slides = [os.path.basename(os.path.dirname(p)) for p in glob(f"{Configs.SLIDES_DIR}/**/ss_thumbnail_thrid_try_predict_224.png", recursive=True)]
+        self.df = self.df[~self.df.slide_uuid.isin(processed_slides)].reset_index(drop=True)
         self.log(f"""ProcessedTileDataset created with {len(self.df)} tiles.""", log_importance=1)
 
     def join_metadata(self, df_pred, inds):
