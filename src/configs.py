237b28afc783ac906414ac7920c90a709b3843f5
SharonPeled <ssharon95@gmail.com>
2023-06-19 23:13:22 +0300

    tile based experiment

--- src/configs.py
+++ src/configs.py
@@ -168,7 +168,7 @@ class SubtypeClassificationConfigs:
     SC_NUM_DEVICES = [1, ]
     SC_DEVICE = 'gpu'
     SC_TRAINING_BATCH_SIZE = 32
-    SC_TRAINING_NUM_WORKERS = 10
+    SC_TRAINING_NUM_WORKERS = 16
     SC_TEST_SIZE = 0.2
     SC_VALID_SIZE = 0.05
     SC_INIT_LR = 1e-4
