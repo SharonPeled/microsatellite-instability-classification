83186a36a5ecddbcd11e2d03ad820d5d22876b08
SharonPeled <ssharon95@gmail.com>
2023-06-29 09:03:11 +0300

    fix CheckpointEveryNSteps.py

--- src/components/objects/CheckpointEveryNSteps.py
+++ src/components/objects/CheckpointEveryNSteps.py
@@ -26,12 +26,14 @@ class CheckpointEveryNSteps(pl.Callback):
         self.save_step_frequency = save_step_frequency
         self.prefix = prefix
         self.use_modelcheckpoint_filename = use_modelcheckpoint_filename
+        self.last_save_global_step = -1
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, unused=0):
         """ Check if we should save a checkpoint after every train batch """
         epoch = trainer.current_epoch
         global_step = trainer.global_step
-        if global_step % self.save_step_frequency == 0:
+        if global_step % self.save_step_frequency == 0 and self.last_save_global_step != global_step:
+            self.last_save_global_step = global_step
             if self.use_modelcheckpoint_filename:
                 filename = trainer.checkpoint_callback.filename
             else:
