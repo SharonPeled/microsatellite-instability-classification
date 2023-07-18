7020005e4ef1c44fb224f50f41d49215312eb32c
SharonPeled <ssharon95@gmail.com>
2023-07-18 17:46:20 +0300

    config change

--- src/configs.py
+++ src/configs.py
@@ -202,7 +202,7 @@ class SubtypeClassificationConfigs:
     # SC_COHORT_TUNE = None # ['COAD', 'READ']
     SC_TEST_ONLY = None
     SC_NUM_EPOCHS = 1
-    SC_NUM_DEVICES = [1, ]
+    SC_NUM_DEVICES = [0, ]
     SC_DEVICE = 'gpu'
     SC_TEST_BATCH_SIZE = 128
     SC_SAVE_CHECKPOINT_STEP_INTERVAL = 10000
