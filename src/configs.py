dede98336dac532be92407de9f6c5ddaa9732c52
SharonPeled <ssharon95@gmail.com>
2023-07-20 23:15:12 +0300

    config change

--- src/configs.py
+++ src/configs.py
@@ -202,8 +202,8 @@ class SubtypeClassificationConfigs:
     # SC_COHORT_TUNE = None # ['COAD', 'READ']
     SC_TEST_ONLY = None
     SC_NUM_EPOCHS = 1
-    SC_NUM_DEVICES = [0, 1]  # for slurm always 0
-    SC_NUM_NODES = 2
+    SC_NUM_DEVICES = [0, ]  # for slurm always 0
+    SC_NUM_NODES = 1
     SC_DEVICE = 'gpu'
     SC_TEST_BATCH_SIZE = 256
     SC_SAVE_CHECKPOINT_STEP_INTERVAL = 10000
