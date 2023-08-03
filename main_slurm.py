7d35b1df85128af019473ec4cdcf581a1a0e9ddc
SharonPeled <ssharon95@gmail.com>
2023-08-03 19:51:57 +0300

    adding rm tmp to main_slurm

--- main_slurm.py
+++ main_slurm.py
@@ -28,6 +28,8 @@ srun -p debug python /home/sharonpe/microsatellite-instability-classification/ma
 echo "Current date and time: $(date)"
 echo "END"
 """)
+    proc = subprocess.Popen(['rm /tmp/* -r -f'], shell=True)
+    proc.wait()
     proc = subprocess.Popen(['sbatch slurm_script.sh'], shell=True)
     proc.wait()
     print('Done.')
