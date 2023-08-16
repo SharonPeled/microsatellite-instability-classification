577dce3bcc5706bee29572dc04c96fc245cc1a3d
SharonPeled <ssharon95@gmail.com>
2023-08-16 18:58:18 +0300

    adding rm tmp file to dino

--- src/configs.py
+++ src/configs.py
@@ -274,7 +274,7 @@ class DINOConfigs:
     DN_NUM_MINI_EPOCHS = 5
     DINO_DICT = {}
     OUT_DIM = 65536
-    DN_BATCH_SIZE = 64  # accumulating gradients in MIL only
+    DN_BATCH_SIZE = 32  # accumulating gradients in MIL only
     DN_NUM_WORKERS = 30
     DN_NUM_EPOCHS = 3*DN_NUM_MINI_EPOCHS
     DN_NUM_DEVICES = [0, ]  # for slurm always 0
