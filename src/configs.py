b0bce93231e5467b486062dd88ed321ae26c703f
SharonPeled <ssharon95@gmail.com>
2023-08-17 12:43:58 +0300

    config change

--- src/configs.py
+++ src/configs.py
@@ -274,8 +274,8 @@ class DINOConfigs:
     DN_NUM_MINI_EPOCHS = 5
     DINO_DICT = {}
     OUT_DIM = 8192
-    DN_BATCH_SIZE = 128
-    DN_NUM_WORKERS = 1
+    DN_BATCH_SIZE = 64
+    DN_NUM_WORKERS = 15
     DN_NUM_EPOCHS = 3*DN_NUM_MINI_EPOCHS
     DN_NUM_DEVICES = [0, ]  # for slurm always 0
     DN_NUM_NODES = 1
