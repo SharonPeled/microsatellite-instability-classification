101c1a31bf0fd856122c2da2066930135e835648
SharonPeled <ssharon95@gmail.com>
2023-08-18 16:36:09 +0300

    preprocess slurm script

---
+++ preprocess_slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=preprocess
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+
+echo "Current date and time: $(date)"
+hostname
+pwd
+
+source /home/sharonpe/miniconda3/etc/profile.d/conda.sh
+conda activate MSI
+
+srun python /home/sharonpe/microsatellite-instability-classification/main.py --preprocess --num-preprocess 10
+
+echo "Current date and time: $(date)"
+echo "END"
