90d9e2fea28f69ad6934774dab7e60c7c52280c9
SharonPeled <ssharon95@gmail.com>
2023-09-05 22:20:13 +0300

    run with submit it using gdx - new pytorch version cuda 12

--- src/components/objects/DINO/main_dino.py
+++ src/components/objects/DINO/main_dino.py
@@ -502,6 +502,8 @@ class DataAugmentationDINO(object):
 
 
 if __name__ == '__main__':
+    if len(Configs.joined) == 0:
+        Configs.set_task_configs(['DN', 'SC'])
     parser = argparse.ArgumentParser('DINO', parents=[get_args_parser()])
     args = parser.parse_args()
     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
