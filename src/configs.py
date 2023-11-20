404c8ed7c7644cceb693d73a72265afa18224ff5
SharonPeled <ssharon95@gmail.com>
2023-11-20 11:28:39 +0200

    cohort learning test

--- src/configs.py
+++ src/configs.py
@@ -162,7 +162,7 @@ class TumorRegressionConfigs:
 class SubtypeClassificationConfigs:
     SC_TILE_SIZE = 512
     SC_EXPERIMENT_NAME = 'SC_COHORT_PREDICTION'
-    SC_FORMULATION = f'p100_512_single_epoch_balanced_tiles'
+    SC_FORMULATION = f'cohort_p100_512_single_epoch_balanced_tiles'
     SC_RUN_NAME = f"{SC_FORMULATION}"
     SC_RUN_DESCRIPTION = f"""Labels are by bioportal.
     """
