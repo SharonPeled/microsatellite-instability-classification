f7f56e29718abe8ce7353401ebbf8f3efc805d42
SharonPeled <ssharon95@gmail.com>
2023-12-04 13:35:13 +0200

    constant bag size

--- src/configs.py
+++ src/configs.py
@@ -194,7 +194,7 @@ class TumorRegressionConfigs:
 class SubtypeClassificationConfigs:
     SC_TILE_SIZE = 512
     SC_EXPERIMENT_NAME = 'SC_DISTILMIL'
-    SC_FORMULATION = f'tile_backbone_sr_100e_4b_3res'
+    SC_FORMULATION = f'tile_backbone_sr_100e_1kb_2res'
     SC_RUN_NAME = f"{SC_FORMULATION}"
     SC_RUN_DESCRIPTION = f"""Labels are by bioportal.
     """
@@ -306,7 +306,7 @@ class SubtypeClassificationConfigs:
         'num_tiles_per_bag': 1000,
         'weight_decay': 1e-4,
         'grad_clip': 5,
-        'num_res_blocks': 3
+        'num_res_blocks': 2
     }
 
 
