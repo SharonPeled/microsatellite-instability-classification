f56babe260380ccec9f98243c2f33b8ca7a48fa4
SharonPeled <ssharon95@gmail.com>
2023-12-05 19:53:54 +0200

    cumulative backward

--- src/configs.py
+++ src/configs.py
@@ -194,7 +194,7 @@ class TumorRegressionConfigs:
 class SubtypeClassificationConfigs:
     SC_TILE_SIZE = 512
     SC_EXPERIMENT_NAME = 'SC_DISTILMIL_2'
-    SC_FORMULATION = f'tile_backbone_sr_w12_5e4_350e_6b_2res'
+    SC_FORMULATION = f'tile_backbone_sr_w16_5e4_400e_6b_3res'
     SC_RUN_NAME = f"{SC_FORMULATION}"
     SC_RUN_DESCRIPTION = f"""Labels are by bioportal.
     """
@@ -232,7 +232,7 @@ class SubtypeClassificationConfigs:
                          'LAB': os.path.join(GeneralConfigs.ROOT, 'data', 'subtype_classification',
                                              f'LAB_statistics_30_512.yaml')}
     SC_CROSS_VALIDATE = True  # num folds according to test size
-    SC_CONTINUE_FROM_FOLD = 1  # 0 to 1/TEST_SIZE
+    SC_CONTINUE_FROM_FOLD = 2  # 0 to 1/TEST_SIZE
     SC_SINGLE_FOLD = False  # 0 to 1/TEST_SIZE
     SC_Y_TO_BE_STRATIFIED = 'y_to_be_stratified'
     SC_CLASS_TO_IND = {'GS': 0, 'CIN': 1} # {'MSS': 0, 'MSI': 1} #
@@ -244,7 +244,7 @@ class SubtypeClassificationConfigs:
     SC_TEST_ONLY = None
     SC_SAVE_TEST = True
     SC_SAVE_TRAIN = False
-    SC_NUM_EPOCHS = 350
+    SC_NUM_EPOCHS = 400
     SC_NUM_DEVICES = 1
     SC_NUM_NODES = 1
     SC_DEVICE = 'gpu'
@@ -304,10 +304,10 @@ class SubtypeClassificationConfigs:
         'attn_dim': 128,
         'num_bags': 6,
         'num_tiles_per_bag': None,
-        'cumulative_batch_size': 12,
+        'cumulative_batch_size': 16,
         'weight_decay': 1e-4,
         'grad_clip': 5,
-        'num_res_blocks': 2
+        'num_res_blocks': 3
     }
 
 
